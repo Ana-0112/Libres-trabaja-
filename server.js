ed925ea4d88d7d5a8747d8cacd432df9e895bc60
MORALESROJASANADALAI <morrojasana0112@gmail.com>
2026-05-12 15:17:52 -0600

    Correccion de error de eliminar

--- server.js
+++ server.js
@@ -869,6 +869,6 @@ const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, '0.0.0.0', () => {
 
-    console.log(`🚀 Servidor activo puerto ${PORT}`);
+    console.log(`Servidor activo puerto ${PORT}`);
 
 });
